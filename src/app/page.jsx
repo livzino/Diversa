e281f30ed7bb68aa36f9bc85a0d70e7f12ab35b3
Liv <livbendix@live.dk>
2024-04-24 10:51:27 +0200

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,10 @@ const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 export default async function Home() {
   return (
     <main>
-      <h1 className={libre.className}>And so it begins...</h1>
+      <h1 className={libre.className}>Product name</h1>
+      <p>Welcome to our web accessibility project! We're using Next.js to create a web application that demonstrates the importance of accessibility and how it aligns with the Web Content Accessibility Guidelines (WCAG) 2.1. By inputting a website URL, users can generate a report highlighting any accessibility issues found, helping website owners and developers ensure their sites are usable by all.</p>
+      <input type="url"></input>
+      <input type="submit" value="Check"></input>
     </main>
   );
 }
