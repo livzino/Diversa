b2bb09f036baf9b8a25b63bc8846fe4bcfb71984
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-04-24 10:53:00 +0200

    apostrof

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <main>
       <h1 className={libre.className}>Product name</h1>
-      <p>Welcome to our web accessibility project! We're using Next.js to create a web application that demonstrates the importance of accessibility and how it aligns with the Web Content Accessibility Guidelines (WCAG) 2.1. By inputting a website URL, users can generate a report highlighting any accessibility issues found, helping website owners and developers ensure their sites are usable by all.</p>
+      <p>Welcome to our web accessibility project! We are using Next.js to create a web application that demonstrates the importance of accessibility and how it aligns with the Web Content Accessibility Guidelines (WCAG) 2.1. By inputting a website URL, users can generate a report highlighting any accessibility issues found, helping website owners and developers ensure their sites are usable by all.</p>
       <input type="url"></input>
       <input type="submit" value="Check"></input>
     </main>
