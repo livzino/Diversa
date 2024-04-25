c6a18aecdc178bdd07d4ae24a6dc76b8a9ecb666
Liv <livbendix@live.dk>
2024-04-25 13:08:33 +0200

    fejlmed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 import { Poppins, Libre_Baskerville } from "next/font/google";
 import "./globals.css";
+import React from "react";
+
+export const dynamic = "force-dynamic";
 
 const libre = Libre_Baskerville({ subsets: ["latin"], weight: "400" });
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
@@ -10,8 +13,8 @@ export default async function Home() {
       <h1 className={libre.className}>Product name</h1>
       <p>Welcome to our web accessibility project! We are using Next.js to create a web application that demonstrates the importance of accessibility and how it aligns with the Web Content Accessibility Guidelines (WCAG) 2.1. By inputting a website URL, users can generate a report highlighting any accessibility issues found, helping website owners and developers ensure their sites are usable by all.</p>
       <form action="/results">
-        <input name="url" type="url" />
-        <button>Check it</button>
+        <input name="url" type="url"></input>
+        <input type="submit" value="Check"></input>
       </form>
     </main>
   );
--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -5,14 +5,30 @@ export const revalidate = 1800;
 
 export default async function Page({ searchParams }) {
   const params = new URLSearchParams(searchParams);
+
   const response = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
   const data = await response.json();
+  console.log(data);
 
   return (
     <main>
       <h1>Report for {data.url}</h1>
       <p>Found {data.violations.length} issues</p>
+      {data.violations.map((violation) => {
+        return <p>{violation.description}</p>;
+      })}
       <Image alt={data.url} src={data.screenshot.url} width={data.screenshot.width} height={data.screenshot.height} />
     </main>
   );
 }
+/*
+{
+      id: 'heading-order',
+      impact: 'moderate',
+      tags: [Array],
+      description: 'Ensures the order of headings is semantically correct',
+      help: 'Heading levels should only increase by one',
+      helpUrl: 'https://dequeuniversity.com/rules/axe/4.9/heading-order?application=axe-puppeteer',
+      nodes: [Array]
+    },
+*/
