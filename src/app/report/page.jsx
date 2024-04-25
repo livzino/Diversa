c962faa79a70e12ce1d4576c109446249db427e7
Liv <livbendix@live.dk>
2024-04-25 13:08:02 +0200

    Fejlmeddelelse

---
+++ src/app/report/page.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+// Revalidate route every 30 minutes
+export const revalidate = 1800;
+
+export default async function Page({ searchParams }) {
+  const params = new URLSearchParams({
+    url: "https://www.charlietango.dk",
+    tags: ["wcag2a", "wcag2aa", "ACT"],
+  });
+  const response = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
+  const data = await response.json();
+
+  return (
+    <main>
+      <h1>Report for {data.url}</h1>
+      <p>Found {data.violations.length} issues</p>
+      <Image alt={data.url} src={data.screenshot.url} width={data.screenshot.width} height={data.screenshot.height} />
+    </main>
+  );
+}
