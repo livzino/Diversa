5ad255bac99b7eeda007d31767759406efcdd8e9
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-02 09:11:44 +0200

    stylet result side og halvt loading

--- src/app/results/loading.js
+++ src/app/results/loading.js
@@ -1,4 +1,16 @@
+"use client";
+
+import "../globals.css";
+
+import { MagnifyingGlass } from "react-loader-spinner";
+
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <div>jeg er et skelet</div>;
+  return (
+    <section className="loading-container">
+      <div id="loading" className="loading-wrapper">
+        <MagnifyingGlass visible={true} height="80" width="80" ariaLabel="magnifying-glass-loading" wrapperStyle={{}} wrapperClass="magnifying-glass-wrapper" glassColor="#c0efff" color="#e15b64" />
+        <p>Scanning page...</p>
+      </div>
+    </section>
+  );
 }
--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -1,10 +1,14 @@
+import "../globals.css";
 import Image from "next/image";
 import { score } from "../components/Grading";
-import "../globals.css";
 import notfound from "../not_found.jsx";
+import { Poppins, Libre_Baskerville } from "next/font/google";
 
 // Revalidate route every 30 minutes
 export const revalidate = 1800;
+const libre = Libre_Baskerville({ subsets: ["latin"], weight: "400" });
+
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export default async function Page({ searchParams }) {
   const params = new URLSearchParams(searchParams);
@@ -16,27 +20,63 @@ export default async function Page({ searchParams }) {
 
   console.log(data);
 
-  const [scoreValue, major, moderate, minor] = score(data);
+  const [scoreValue] = score(data);
 
   return (
     <main className="App">
-      <div className="dataCard revenueCard">
-        <h1>Report for {data.url}</h1>
-        <p>Found {data.violations.length} issues</p>
+      <div className="card textCard">
+        <h1 className={libre.className}>Report for {data.url}</h1>
+        {/*  <p>Found {data.violations.length} issues</p> */}
         {data.violations.map((violation) => {
-          return <p key={data}>{violation.impact}</p>;
+          let color;
+
+          if (violation.impact === "minor") {
+            color = "RGB(153, 255, 153)";
+          } else if (violation.impact === "moderate") {
+            color = "RGB(255, 255, 153)";
+          } else if (violation.impact === "serious") {
+            color = "RGB(255, 102, 102)";
+          } else {
+            color = "RGB(255, 102, 102)";
+          }
+          return (
+            <ul key={data} className={poppins.className}>
+              <li>
+                <p className="listTitle" style={{ backgroundColor: color, display: "inline-block", padding: "0 5px" }}>
+                  {violation.impact}
+                </p>
+                <p>{violation.description}</p>
+              </li>
+            </ul>
+          );
         })}
       </div>
 
-      <div className="dataCard customerCard">
+      <div className="card imgCard">
         <Image alt={data.url} src={data.screenshot.url} width={data.screenshot.width} height={data.screenshot.height} />
       </div>
 
-      <div className="dataCard categoryCard">
-        <p>Score: {scoreValue}%</p>
-        <p>Major: {major.length}</p>
+      <div className="card gradingCard ">
+        <p className={`${libre.className} listTitle`}> Score: {isNaN(scoreValue) ? "Not Available" : `${scoreValue}%`}</p>{" "}
+        {(() => {
+          if (scoreValue >= 90) {
+            return <Image className="badge" alt="A" src="/assets/grade1.png" width={50} height={50} />;
+          } else if (scoreValue >= 80) {
+            return <Image className="badge" alt="B" src="/assets/grade2.png" width={50} height={50} />;
+          } else if (scoreValue >= 70) {
+            return <Image className="badge" alt="C" src="/assets/grade3.png" width={50} height={50} />;
+          } else if (scoreValue >= 60) {
+            return <Image className="badge" alt="D" src="/assets/grade4.png" width={50} height={50} />;
+          } else if (scoreValue <= 60) {
+            return <Image className="badge" alt="F" src="/assets/grade5.png" width={50} height={50} />;
+          } else {
+            return <Image className="badge" alt="N/A" src="/assets/none.png" width={50} height={50} />;
+          }
+        })()}{" "}
+        {/*   <p>Major: {major.length}</p>
         <p>Moderate: {moderate.length}</p>
-        <p>Minor: {minor.length}</p>
+        <p>Minor: {minor.length}</p> */}
+        <button className="btn">Grading guidelines</button>
       </div>
     </main>
   );
