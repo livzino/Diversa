e36df92778ecef3219c7dd20fd3a89958da34477
Liv <livbendix@live.dk>
2024-05-02 11:20:27 +0200

    fejk

--- src/app/not_found.jsx
+++ src/app/not_found.jsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import "../app/globals.css";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <main className="mainnot">
+      <div className="main">
+        <p className="diversa"> Diversa</p>
+        <h1 className="h1not">404</h1>
+        <h2 className="h2not">Not Found</h2>
+        <p className="pte">Could not find requested resource</p>
+        <Link href="/" className="notbutn">
+          Return Home
+        </Link>{" "}
+      </div>
+    </main>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import { Poppins, Libre_Baskerville } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import "../app/globals.css";
 
 export const dynamic = "force-dynamic";
 
@@ -9,12 +10,14 @@ const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export default async function Home() {
   return (
-    <main>
-      <h1 className={libre.className}>Product name</h1>
-      <p>Welcome to our web accessibility project! We are using Next.js to create a web application that demonstrates the importance of accessibility and how it aligns with the Web Content Accessibility Guidelines (WCAG) 2.1. By inputting a website URL, users can generate a report highlighting any accessibility issues found, helping website owners and developers ensure their sites are usable by all.</p>
+    <main className="pagemain">
+      <p className="diversa"> Diversa</p>
+      <h1 className={`${libre.className} h1page`}>Product name</h1>
+      <p className="ppage">Welcome to our web accessibility project! We are using Next.js to create a web application that demonstrates the importance of accessibility and how it aligns with the Web Content Accessibility Guidelines (WCAG) 2.1. By inputting a website URL, users can generate a report highlighting any accessibility issues found, helping website owners and developers ensure their sites are usable by all.</p>
       <form action="/results">
-        <input name="url" type="url"></input>
-        <input type="submit" value="Check"></input>
+        <input name="url" type="url" className="url"></input>
+        <br></br>
+        <input type="submit" value="Check" className="check"></input>
       </form>
     </main>
   );
