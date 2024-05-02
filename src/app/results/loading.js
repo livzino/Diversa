837c03f22137f724851e1a9aeab1f2bb735624a2
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-02 11:17:59 +0200

    færdig med styling ish

--- src/app/results/loading.js
+++ src/app/results/loading.js
@@ -1,15 +1,17 @@
 "use client";
+import { Poppins, Libre_Baskerville } from "next/font/google";
 
 import "../globals.css";
 
 import { MagnifyingGlass } from "react-loader-spinner";
+const libre = Libre_Baskerville({ subsets: ["latin"], weight: "400" });
 
 export default function Loading() {
   return (
     <section className="loading-container">
       <div id="loading" className="loading-wrapper">
-        <MagnifyingGlass visible={true} height="80" width="80" ariaLabel="magnifying-glass-loading" wrapperStyle={{}} wrapperClass="magnifying-glass-wrapper" glassColor="#c0efff" color="#e15b64" />
-        <p>Scanning page...</p>
+        <MagnifyingGlass visible={true} height="100" width="100" ariaLabel="magnifying-glass-loading" wrapperStyle={{}} wrapperClass="magnifying-glass-wrapper" glassColor="#c0efff" color="#fff" />
+        <p className={`${libre.className} loading-title`}>Scanning page...</p>
       </div>
     </section>
   );
