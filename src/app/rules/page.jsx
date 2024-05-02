aee94216e479c8b56e030b482ef8585d622af256
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-02 13:50:10 +0200

    tester

--- src/app/rules/page.jsx
+++ src/app/rules/page.jsx
@@ -1,53 +1,56 @@
-/* import "../globals.css";
+import "../globals.css";
+import "../rules/rules.js";
 import { Poppins, Libre_Baskerville } from "next/font/google";
+import { accordion } from "../rules/rules.js";
 
 const libre = Libre_Baskerville({ subsets: ["latin"], weight: "400" });
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
-export default async function Home() {
+export default async function Rules() {
   return (
     <main className="pagemain">
       <h1 className={`${libre.className} h1rules`}>WCAG 2.0 Level A & AA Rules</h1>
-      <p className="ppage">Welcome to our web accessibility project! We are using Next.js to create a web application that demonstrates the importance of accessibility and how it aligns with the Web Content Accessibility Guidelines (WCAG) 2.1. By inputting a website URL, users can generate a report highlighting any accessibility issues found, helping website owners and developers ensure their sites are usable by all.</p>
-      <form action="/">
-        <input name="url" type="url" className="url"></input>
-        <br></br>
-        <input type="submit" value="Check" className="check"></input>
-      </form>
+      <div class="accordion">
+        <div class="content-box">
+          <div class="label">Area-alt</div>
+
+          <div class="content">
+            <p>Ensures area elements of image maps have alternate text</p>
+
+            <p>Critical</p>
+          </div>
+        </div>
+
+        <div class="content-box">
+          <div class="label">aria-braille-equivalent</div>
+
+          <div class="content">
+            <p>Ensure aria-braillelabel and aria-brailleroledescription have a non-braille equivalent</p>
+
+            <p>Serious</p>
+          </div>
+        </div>
+
+        <div class="content-box">
+          <div class="label">form-field-multiple-labels</div>
+
+          <div class="content">
+            <p>Ensures form field does not have multiple label elements</p>
+
+            <p>Moderate</p>
+          </div>
+        </div>
+
+        <div class="content-box">
+          <div class="label">server-side-image-map</div>
+
+          <div class="content">
+            <p>Ensures that server-side image maps are not used</p>
+
+            <p>Minor</p>
+          </div>
+        </div>
+      </div>
     </main>
-  );
-}
- */
-
-import Link from "next/link";
-
-export default async function Rules() {
-  const url = "https://dequeuniversity.com/rules/axe/4.9";
-  const res = await fetch(url);
-
-  const dogs = await res.json();
-
-  console.log(dogs);
-
-  return (
-    <nav className="bg-black text-white p-2">
-      <ul className="md:flex gap-2">
-        <li>
-          <Link href={"/"} prefetch={false}>
-            Home
-          </Link>
-        </li>
-        {dogs.map((dog) => {
-          const { id, description, impact, slug } = dog;
-          return (
-            <li key={slug}>
-              <Link href={`/${slug}`} prefetch={false}>
-                {name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
   );
 }
---
+++ src/app/rules/rules.js
@@ -0,0 +1,8 @@
+import "../rules/page.jsx";
+
+export const accordion = document.getElementsByClassName("content-box");
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
